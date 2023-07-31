8f0a7db6323aba009cbf36a8d6a13fab3eee2954
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-31 19:40:46 +0300

    details

--- car-catalog/src/CarApp.js
+++ car-catalog/src/CarApp.js
@@ -47,3 +47,35 @@ const CarApp = () => {
 
 export default CarApp;
 
+// CarApp.js
+import React, { useState, useEffect } from "react";
+
+const CarCard = ({ car }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <div className={`car-card${showDetails ? " active" : ""}`} onClick={() => setShowDetails(!showDetails)}>
+      <img src={car.img_url} alt={car.model} />
+      <h3>{car.brand} - {car.model}</h3>
+      {showDetails && (
+        <div className="details">
+          <p>Type: {car.type}</p>
+          <p>Price: {car.price}</p>
+          <p>Engine Type: {car.engine_type}</p>
+          <p>Acceleration: {car.acceleration}</p>
+          <p>Maximum Speed: {car.maximum_speed}</p>
+          <p>Release Date: {car.release_date}</p>
+        </div>
+      )}
+      <div className={`show-details-btn${showDetails ? " active" : ""}`}>{showDetails ? "Hide Details" : "Show Details"}</div>
+    </div>
+  );
+};
+
+const CarApp = () => {
+  
+};
+
+export default CarApp;
+
+
