5fff191248a0d1b90113ef6795b81e22d48c3c69
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-31 19:45:23 +0300

    car components done

--- car-catalog/src/CarApp.js
+++ car-catalog/src/CarApp.js
@@ -1,53 +1,3 @@
-// CarApp.js
-import React, { useState, useEffect } from "react";
-
-const CarApp = () => {
-  return (
-    <div className="car-app">
-      {}
-    </div>
-  );
-};
-
-export default CarApp;
-
-
-import React, { useState, useEffect } from "react";
-
-const CarCard = ({ car }) => {
-  return (
-    <div className="car-card">
-      <img src={car.img_url} alt={car.model} />
-      <h3>{car.brand} - {car.model}</h3>
-    </div>
-  );
-};
-
-const CarApp = () => {
-  const [cars, setCars] = useState([]);
-
-  useEffect(() => {
-    
-    fetch("https://api.npoint.io/6b49fadc38eab9e79911")
-      .then((response) => response.json())
-      .then((data) => setCars(data.cars))
-      .catch((error) => console.log(error));
-  }, []);
-
-  return (
-    <div className="car-app">
-      <div className="car-list">
-        {cars.map((car) => (
-          <CarCard key={car.model} car={car} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default CarApp;
-
-// CarApp.js
 import React, { useState, useEffect } from "react";
 
 const CarCard = ({ car }) => {
@@ -73,28 +23,25 @@ const CarCard = ({ car }) => {
 };
 
 const CarApp = () => {
-  
-};
-
-export default CarApp;
-
-// CarApp.js
-import React, { useState, useEffect } from "react";
-
-const CarCard = ({ car }) => {
-  // Same as before
-};
-
-const CarApp = () => {
-  const [brandFilter, setBrandFilter] = useState("");
+  const [cars, setCars] = useState([]);
   const [filteredCars, setFilteredCars] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [brandFilter, setBrandFilter] = useState("");
 
   useEffect(() => {
-   
+    // Fetch data from the API
+    fetch("https://api.npoint.io/6b49fadc38eab9e79911")
+      .then((response) => response.json())
+      .then((data) => {
+        setCars(data.cars);
+        setFilteredCars(data.cars);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   useEffect(() => {
-    
+    // Filter cars based on brand
     if (brandFilter) {
       const filtered = cars.filter((car) => car.brand === brandFilter);
       setFilteredCars(filtered);
@@ -103,10 +50,60 @@ const CarApp = () => {
     }
   }, [brandFilter, cars]);
 
+  useEffect(() => {
+    // Sort cars based on selected criteria
+    if (sortBy === "brand_asc") {
+      setFilteredCars([...filteredCars].sort((a, b) => a.brand.localeCompare(b.brand)));
+    } else if (sortBy === "brand_desc") {
+      setFilteredCars([...filteredCars].sort((a, b) => b.brand.localeCompare(a.brand)));
+    } else if (sortBy === "price_high") {
+      setFilteredCars([...filteredCars].sort((a, b) => parseFloat(b.price.split(" ")[1]) - parseFloat(a.price.split(" ")[1])));
+    } else if (sortBy === "price_low") {
+      setFilteredCars([...filteredCars].sort((a, b) => parseFloat(a.price.split(" ")[1]) - parseFloat(b.price.split(" ")[1])));
+    } else if (sortBy === "speed_high") {
+      setFilteredCars([...filteredCars].sort((a, b) => parseFloat(b.maximum_speed.split(" ")[0]) - parseFloat(a.maximum_speed.split(" ")[0])));
+    } else if (sortBy === "speed_low") {
+      setFilteredCars([...filteredCars].sort((a, b) => parseFloat(a.maximum_speed.split(" ")[0]) - parseFloat(b.maximum_speed.split(" ")[0])));
+    }
+  }, [sortBy, filteredCars]);
+
+  useEffect(() => {
+    // Search functionality
+    if (searchTerm) {
+      const filtered = cars.filter((car) =>
+        car.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        car.model.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredCars(filtered);
+    } else {
+      setFilteredCars(cars);
+    }
+  }, [searchTerm, cars]);
+
   return (
     <div className="car-app">
       <div className="filter-section">
-        {}
+        <input
+          type="text"
+          placeholder="Search by brand or model"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <select value={brandFilter} onChange={(e) => setBrandFilter(e.target.value)}>
+          <option value="">All Brands</option>
+          {Array.from(new Set(cars.map((car) => car.brand))).map((brand) => (
+            <option key={brand} value={brand}>{brand}</option>
+          ))}
+        </select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="">Sort By</option>
+          <option value="brand_asc">Brand (A-Z)</option>
+          <option value="brand_desc">Brand (Z-A)</option>
+          <option value="price_high">Price (High to Low)</option>
+          <option value="price_low">Price (Low to High)</option>
+          <option value="speed_high">Maximum Speed (High to Low)</option>
+          <option value="speed_low">Maximum Speed (Low to High)</option>
+        </select>
       </div>
       <div className="car-list">
         {filteredCars.map((car) => (
@@ -118,6 +115,3 @@ const CarApp = () => {
 };
 
 export default CarApp;
-
-
-
