d7a8debd84a84a2977552b7ed852822b70cb64c3
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-31 19:32:09 +0300

    first setup

---
+++ car-catalog/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CarApp from './CarApp';
+
+function App() {
+  return (
+    <div className="App">
+      <CarApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ car-catalog/src/CarApp.js
@@ -0,0 +1,12 @@
+// CarApp.js
+import React, { useState, useEffect } from "react";
+
+const CarApp = () => {
+  return (
+    <div className="car-app">
+      {}
+    </div>
+  );
+};
+
+export default CarApp;
